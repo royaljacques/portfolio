c0b5c8bc99699173bf1f40df947fcd8c341bafc5
royaljacquesytb <maximejacques667@gmail.com>
2024-02-29 17:07:13 +0100

    add carrousel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+import webpack from 'webpack';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    webpack: (config, options) => {
+        config.plugins.push(
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+        })
+        );
+        return config;
+    },
 };
-
+//https://medium.com/geekculture/using-owl-carousel-with-nextjs-e63a166225b0
 export default nextConfig;
--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,11 +1,32 @@
-import makeStyles from "@mui/material/styles/makeStyles";
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Typography from "@mui/material/Typography";
 import React from "react";
 import classes from "./style/style.module.css"
-export default function About(){
+import dynamic from "next/dynamic";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import Image from "next/image";
+
+const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
+	ssr: false,
+});
+const Responsive = {
+	0: {
+		items: 2,
+		margin: 5
+	},
+	768: {
+		items: 3,
+		margin: 10
+	},
+	1024: {
+		items: 4,
+		margin: 20,
+	}
+}
+const About = () => {
 
 	return (
 		<div className={classes.heroContent}>
@@ -18,7 +39,7 @@ export default function About(){
 					likes to discover new ways of developing
 				</Typography>
 				<div className={classes.heroButtons}>
-					<Grid container={true} spacing={2} sx={{justifyContent:"center", alignItems:"center"}}>
+					<Grid container={true} spacing={2} sx={{justifyContent: "center", alignItems: "center"}}>
 						<Grid item>
 							<Button href="./cv" target="_blank" variant="contained"
 							        color="primary">
@@ -34,6 +55,46 @@ export default function About(){
 					</Grid>
 				</div>
 			</Container>
+			<Container maxWidth="md" sx={{marginTop: "5vh"}}>
+				<OwlCarousel
+					className="owl-theme"
+					loop
+					margin={10}
+					items={5}
+					autoplay={true}
+					autoplayTimeout={3000}
+					autoplayHoverPause={true}
+					responsive={Responsive}
+				>
+					<div className="item">
+						<Image src={require("../assets/images/reactLogo.svg")} alt="image1" width={200} height={200}
+						/>
+					</div>
+					<div className="item">
+						<Image src={require("../assets/images/Discord.js.svg")} alt="image1" width={200} height={200}
+						/>
+					</div>
+					<div className="item">
+						<Image src={require("../assets/images/node-js.svg")} alt="image1" width={200} height={200}
+						/>
+					</div>
+					<div className="item">
+						<Image src={require("../assets/images/fastify.svg")} alt="image1" width={200} height={200}/>
+					</div>
+					<div className="item">
+						<Image src={require("../assets/images/next-js.svg")} alt="image1" width={200} height={200}/>
+					</div>
+					<div className="item">
+						<Image src={require("../assets/images/pocketmine.png")} alt="image1" width={200} height={200}
+						/>
+					</div>
+					<div className="item">
+						<Image src={require("../assets/images/php.svg")} alt="image1" width={200} height={200}
+						/>
+					</div>
+				</OwlCarousel>
+			</Container>
 		</div>
-	)
+	);
 }
+export default About;
